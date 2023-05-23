f6366f16fae5d025da4353bded2cde49a3a90de6
Timothy <timkhumpan@gmail.com>
2023-05-23 02:24:40 -0400

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,44 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
 import Pet from "./Pet";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
+import Mdal from "./Mdal";
 
-// delete the Pet component
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+            <Route
+              path="/spotify"
+              element={
+                <Mdal
+                  heading={"I Love You Mikasa"}
+                  picture={
+                    "https://static.wikia.nocookie.net/shingekinokyojin/images/3/3c/Eren_Jaeger_%28Anime%29_character_image_%28850%29.png/revision/latest?cb=20201228000236"
+                  }
+                  name={"Eren Jaeger"}
+                />
+              }
+            />
+          </Routes>
+        </QueryClientProvider>
       </BrowserRouter>
     </div>
   );
